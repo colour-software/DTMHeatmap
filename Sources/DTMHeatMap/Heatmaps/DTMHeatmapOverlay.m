0423ced6b88842d7ae1f447752a1c5cca0993b4e
Alex Cason <alex@cason.co.uk>
2022-08-19 13:57:28 +0100

    Change zoom scale calculation

--- Sources/DTMHeatMap/Heatmaps/DTMHeatmapOverlay.m
+++ Sources/DTMHeatMap/Heatmaps/DTMHeatmapOverlay.m
@@ -101,8 +101,7 @@
     NSMutableDictionary *toReturn = [[NSMutableDictionary alloc] init];
     int bucketDelta = self.screenPointsPerBucket / scale;
     
-//    double zoomScale = log2(1/scale);
-    double zoomScale = 1/scale;
+    double zoomScale = log2(0.5/scale);
     double slope = (self.zoomedOutMax - self.maxValue) / (kSBZoomLevels - 1);
     double x = pow(zoomScale, self.scalePower) / pow(kSBZoomLevels, self.scalePower - 1);
     double scaleFactor = (x - 1) * slope + self.maxValue;
